f69fc313822ceed7ebd853c65f9d12f592a4cde4
Eric S Pan <epan@stoamedical.io>
2025-03-20 12:37:05 -0700

    Update logo

---
+++ app/src/components/Navbar.svelte
@@ -0,0 +1,15 @@
+<script>
+    export let img_text = "Stoa Medical's logo — the greek letter sigma (Σ) and the Rod of Asclepius.";
+</script>
+
+<div>
+    <a href="/">
+        <img class="bg-zinc-50 rounded-md" src="/Logomark.svg" alt={img_text} width="32rem" height="32rem"/>
+    </a>
+    <nav>
+        <ul>
+            <li><a class="border-b-2" href="/about">about</a></li>
+            <li><a class="border-b-2" href="/projects">projects</a></li>
+        </ul>
+    </nav>
+</div>
--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,20 +1,12 @@
 <!-- src/routes/+layout.svelte -->
 <script>
     import '../app.css';
-    let img_text = "Stoa Medical's logo — the greek letter sigma (Σ) and the Rod of Asclepius.";
+    import Navbar  from '../components/Navbar.svelte'
 </script>
 
 <div class="container">
     <aside class="sidebar m-4">
-        <a href="/">
-            <img class="bg-zinc-50 rounded-md" src="/logo.png" alt={img_text} width="64rem" height="64rem"/>
-        </a>
-        <nav>
-            <ul>
-                <li><a class="border-b-2" href="/about">about</a></li>
-                <li><a class="border-b-2" href="/projects">projects</a></li>
-            </ul>
-        </nav>
+        <Navbar />
     </aside>
     <main>
         <div class="h-8"></div>
