802c3f07b04b437a6f1522ce030c4cf2d125e64c
Eric Pan <epan@stoamedical.io>
2025-04-02 09:16:32 -0700

    Fix typo

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -5,5 +5,5 @@
 <main>
     <h1>Stoa Medical is an open-core software agency integrating biomedicine and wellness</h1>
     <br>
-    <p>We're <a href="https://en.wikipedia.org/wiki/Bootstrapping#Business">bootstraping</a> — check-back for updates!</p>
+    <p>We're <a href="https://en.wikipedia.org/wiki/Bootstrapping#Business">bootstrapping</a> — check back for updates!</p>
 </main>
